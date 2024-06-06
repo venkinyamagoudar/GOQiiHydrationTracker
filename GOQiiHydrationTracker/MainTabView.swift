e798b5960c80c9fe295519d2c9f298686688e8f3
Venkatesh Nyamagoudar <vnyamagoudar@gmail.com>
2024-06-06 18:37:11 +0530

    Initial Commit, Changed the file name from "ContentView" to "MainTabView".

--- GOQiiHydrationTracker/MainTabView.swift
+++ GOQiiHydrationTracker/MainTabView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainTabView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    MainTabView()
 }
