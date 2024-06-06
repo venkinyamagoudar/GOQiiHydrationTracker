6e0f642bb89ab6cc2c064b82963d542d1d50180b
Venkatesh Nyamagoudar <vnyamagoudar@gmail.com>
2024-06-06 18:12:48 +0530

    Initial Commit

---
+++ GOQiiHydrationTracker/GOQiiHydrationTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GOQiiHydrationTrackerApp.swift
+//  GOQiiHydrationTracker
+//
+//  Created by Venkatesh Nyamagoudar on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct GOQiiHydrationTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
