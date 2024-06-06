c47ef57cd2c97f31c069b73fd23e3cf0bf2538e5
Venkatesh Nyamagoudar <vnyamagoudar@gmail.com>
2024-06-07 00:16:23 +0530

    Impleented Notifications

---
+++ GOQiiHydrationTracker/HomeView/View/NotificatioView/HyderationNotificationView.swift
@@ -0,0 +1,18 @@
+//
+//  HyderationNotificationView.swift
+//  GOQiiHydrationTracker
+//
+//  Created by Venkatesh Nyamagoudar on 6/7/24.
+//
+
+import SwiftUI
+
+struct HyderationNotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HyderationNotificationView()
+}
---
+++ GOQiiHydrationTracker/Manager/NotificationManager.swift
@@ -0,0 +1,8 @@
+//
+//  NotificationManager.swift
+//  GOQiiHydrationTracker
+//
+//  Created by Venkatesh Nyamagoudar on 6/7/24.
+//
+
+import Foundation
