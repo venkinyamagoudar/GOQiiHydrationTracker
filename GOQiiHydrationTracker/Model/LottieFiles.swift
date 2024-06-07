7fb7813fc953d3a485eb04a1912f5ffab44c949d
Venkatesh Nyamagoudar <vnyamagoudar@gmail.com>
2024-06-07 16:14:26 +0530

    Refactoring the code

--- GOQiiHydrationTracker/Model/LottieFiles.swift
+++ GOQiiHydrationTracker/Model/LottieFiles.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+enum LottieFiles {
+    static var noItemsPresent = "NoItemsPresent"
+    static var noDataFound = "NoDataFound"
+}
+
+extension Date {
+    func toString(dateStyle: DateFormatter.Style, timeStyle: DateFormatter.Style) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = dateStyle
+        dateFormatter.timeStyle = timeStyle
+        return dateFormatter.string(from: self)
+    }
+    
+    func previousSevenDaysDates() -> [String] {
+        var dates: [String] = []
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .none
+        
+        for day in 1...7 {
+            if let previousDate = Calendar.current.date(byAdding: .day, value: -day, to: self) {
+                let dateString = previousDate.toString(dateStyle: .short, timeStyle: .none)
+                dates.append(dateString)
+            }
+        }
+        
+        return dates
+    }
+}
